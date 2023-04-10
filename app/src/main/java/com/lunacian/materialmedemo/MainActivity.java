c244191cd8bc99299c4808759ea22b5960f2c903
noriannelamadrid27 <noriannelamadrid@cnsc.edu.ph>
2023-04-10 15:55:48 +0800

    initial commit

---
+++ app/src/main/java/com/lunacian/materialmedemo/MainActivity.java
@@ -0,0 +1,62 @@
+package com.lunacian.materialmedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Member variables.
+    private RecyclerView mRecyclerView;
+    private ArrayList<Sport> mSportsData;
+    private SportsAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerView);
+
+        // Set the Layout Manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the ArrayList that will contain the data.
+        mSportsData = new ArrayList<>();
+
+        // Initialize the adapter and set it to the RecyclerView.
+        mAdapter = new SportsAdapter(this, mSportsData);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Get the data.
+        initializeData();
+    }
+
+    /**
+     * Initialize the sports data from resources.
+     */
+    private void initializeData() {
+        // Get the resources from the XML file.
+        String[] sportsList = getResources()
+                .getStringArray(R.array.sports_titles);
+        String[] sportsInfo = getResources()
+                .getStringArray(R.array.sports_info);
+
+        // Clear the existing data (to avoid duplication).
+        mSportsData.clear();
+
+        // Create the ArrayList of Sports objects with titles and
+        // information about each sport.
+        for(int i=0;i<sportsList.length;i++){
+            mSportsData.add(new Sport(sportsList[i],sportsInfo[i]));
+        }
+
+        // Notify the adapter of the change.
+        mAdapter.notifyDataSetChanged();
+    }
+}
